f46eebf657ac52b5407f2ced660222fb187e6822
Manahel24 <manahelariellaperta@gmail.com>
2026-05-22 14:14:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("🎈WELCOME VILLAGGERS")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("HELLO NEWBIE")
+st.title("THIS IS YOUR TIME :blue[cool] :sunglasses:")
